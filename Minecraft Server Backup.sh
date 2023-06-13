2d478d6fc52b174a8a74b8a9bc92da26226a6f2a
True-Mousse <101472540+True-Mousse@users.noreply.github.com>
2023-06-13 10:48:29 -0700

    Comments Thank You
    Added link to mccron github link & reference to original creator

--- Minecraft Server Backup.sh
+++ Minecraft Server Backup.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
+##################################################################
+# Script used to backup MC server to external HDD.
+# Backups older than 14x days are removed. 
+#
+# mcrcon package is provided by: https://github.com/Tiiffi/mcrcon
+#
+# A thank you to Nicky Matthew for providing a tutorial and examples @ 
+# https://bobcares.com/blog/install-minecraft-server-on-ubuntu/ 
+#  
+##################################################################
 
-function rcon 
-{
-/opt/minecraft/tools/mcrcon/mcrcon -H 127.0.0.1 -P 25575 -p PASSWORD “$1”
+rcon(){
+     /opt/minecraft/tools/mcrcon/mcrcon -H 127.0.0.1 -P 25575 -p STRONG-PASSWORD "$1"
 }
 
 ## Minecraft command that disables the server writing to the world files.
-rcon “save-off”
+rcon "save-off"
 
 ## Minecraft command that saves the server to the data storage device.
-rcon “save-all”
+rcon "save-all"
 
-
-tar -czf /opt/minecraft/backups/server-$(date +%F-%H-%M).tar.gz /opt/minecraft/server
+tar -czf /mnt/spare1/minecraft/backups/The-World-$(date +%F-%H-%M).tgz /opt/minecraft
 
 ## Minecraft command that enables server writing to the world files.
-rcon “save-on”
+rcon "save-on"
 
-## Delete older backups
-find /opt/minecraft/backups/ -type f -mtime +14 -name '*.gz' -delete
+## Delete backups older than 14 days
+find /mnt/spare1/minecraft/backups/ -type f -mtime +14 -name '*.tgz' -delete
